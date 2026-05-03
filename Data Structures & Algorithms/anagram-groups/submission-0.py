18895b3315683e30131b917b70e0c19615a96ed1
Genevieve <lee.jiawei@proton.me>
2026-05-03 22:00:11 +0800

    Delete Data Structures & Algorithms/anagram-groups directory

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict = defaultdict(list)
-        for i in strs:
-            dict["".join(sorted(i))].append(i)
-        return list(dict.values())
